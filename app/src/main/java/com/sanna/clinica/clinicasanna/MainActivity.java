3fca93a4d1e9d05875e7b5159f02f43f431403e2
scott <scottperezquispe@gmail.com>
2018-04-21 18:28:42 -0500

    Actulización de iconos

--- app/src/main/java/com/sanna/clinica/clinicasanna/MainActivity.java
+++ app/src/main/java/com/sanna/clinica/clinicasanna/MainActivity.java
@@ -1,16 +1,33 @@
 package com.sanna.clinica.clinicasanna;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private boolean bandera = false;
+    private Button btnIngreso;
+    private EditText txt_usuario, txt_clave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This is my tes code
+        btnIngreso = (Button) findViewById(R.id.btn_ingreso);
+
+
+        btnIngreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
