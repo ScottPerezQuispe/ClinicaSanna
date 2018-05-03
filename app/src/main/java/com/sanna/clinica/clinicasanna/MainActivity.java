7619a717703f01a4b82f3b9b55cd8c9c2547a75a
scott <scottperezquispe@gmail.com>
2018-05-03 01:07:06 -0500

    Actualizar view

--- app/src/main/java/com/sanna/clinica/clinicasanna/MainActivity.java
+++ app/src/main/java/com/sanna/clinica/clinicasanna/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sanna.clinica.clinicasanna;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,21 +14,54 @@ public class MainActivity extends AppCompatActivity {
     private boolean bandera = false;
     private Button btnIngreso;
     private EditText txt_usuario, txt_clave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnIngreso = (Button) findViewById(R.id.btn_ingreso);
-
+        txt_usuario = (EditText) findViewById(R.id.txt_usuario);
+        txt_clave = (EditText) findViewById(R.id.txt_clave);
 
         btnIngreso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (bandera == false) {
+                    bandera = true;
+                    String user = txt_usuario.getText().toString();
+                    boolean validator = true;
+
+                    if (user.equals("") /* ||  pass.equals("")*/) {
+                       /* Toast.makeText(getApplication(), "Ingrese su DNI", Toast.LENGTH_SHORT).show();*/
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                        builder.setMessage("Ingrese DNI");
+                        builder.setTitle("Advertencia");
+                        builder.setNegativeButton("Aceptar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog dialog=builder.create();
+                        dialog.show();
+
+                        txt_usuario.setFocusable(true);
+                        validator = false;
+                        bandera = false;
+                    }
+
+                    if (validator == true) {
+                       /* ValidarLogin login = new ValidarLogin();
+                        login.execute(user, pass);*/
+                        Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
 
 
-                Intent intent = new Intent(MainActivity.this,MenuActivity.class);
-                startActivity(intent);
             }
         });
     }
